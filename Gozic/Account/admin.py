e6a32424495ed2f10320ac85cae64e67c756e841
XDang04 <hod.g204@gmail.com>
2025-06-05 23:16:48 +0700

    update login

--- Gozic/Account/admin.py
+++ Gozic/Account/admin.py
@@ -1,22 +1,22 @@
-from django.contrib import admin
-from .models import Account
+# from django.contrib import admin
+# from .models import Account
 
 
 
 
-fieldsets = (
-    (None, {'fields': ('username', 'password')}),
-    ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'phone', 'birthday', 'position', 'location', 'company', 'skype', 'image')}),
-    ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-    ('Important dates', {'fields': ('last_login', 'date_joined')}),
-)
-add_fieldsets = (
-    (None, {
-        'classes': ('wide',),
-        'fields': ('username', 'password1', 'password2', 'email', 'first_name', 'last_name', 'phone', 'birthday', 'position', 'location', 'company', 'skype', 'image'),
-    }),
-)
+# fieldsets = (
+#     (None, {'fields': ('username', 'password')}),
+#     ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'phone', 'birthday', 'position', 'location', 'company', 'skype', 'image')}),
+#     ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+#     ('Important dates', {'fields': ('last_login', 'date_joined')}),
+# )
+# add_fieldsets = (
+#     (None, {
+#         'classes': ('wide',),
+#         'fields': ('username', 'password1', 'password2', 'email', 'first_name', 'last_name', 'phone', 'birthday', 'position', 'location', 'company', 'skype', 'image'),
+#     }),
+# )
 
 
 
-admin.site.register(Account)
+# admin.site.register(Account)
