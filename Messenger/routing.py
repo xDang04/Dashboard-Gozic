71d66468373dfc966e656cddefe5bb214143ea89
XDang04 <hod.g204@gmail.com>
2025-06-05 16:29:11 +0700

    fix conflict

--- Messenger/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import consumers
-
-websocket_urlpatterns = [
-    path('messenger/ws/chatroom/<str:chatroom_name>', consumers.ChatRoomConsumer.as_asgi()),
-]
--- Messenger/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path, include
-from Messenger.views import messenger
-urlpatterns = [
-    path('', messenger, name='messenger'),
-]
